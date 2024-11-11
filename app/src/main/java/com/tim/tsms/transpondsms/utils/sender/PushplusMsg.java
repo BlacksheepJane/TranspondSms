270f6df97da87ac70fb4b09f0a52cfec8185ceb8
Yeal <1811038806@sjtu.edu.cn>
2024-11-11 22:18:27 +0800

    删除dingding,网页，微信机器人的layout对应的xml 修改对应activity确保能够运行 提交反馈将直接将消息发送到WL的邮箱 检查版本将直接显示没有新版本，没有其他代码逻辑

---
+++ app/src/main/java/com/tim/tsms/transpondsms/utils/sender/PushplusMsg.java
@@ -0,0 +1,62 @@
+package com.tim.tsms.transpondsms.utils.sender;
+
+import android.util.Log;
+
+
+import java.io.IOException;
+
+import com.tim.tsms.transpondsms.utils.SettingUtil;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+public class PushplusMsg {
+
+    static String TAG = "PushplusMsg";
+
+    public static void sendMsg(String msg) throws Exception{
+
+        String pushPlusToken = SettingUtil.get_using_pushplus_token();
+        if (pushPlusToken.equals("")) {
+            return;
+        }
+
+        // 构建 webhook URL
+        String webhookUrl = "http://www.pushplus.plus/send?token=" + pushPlusToken;
+
+        final String msgf = msg;
+
+        // 构建消息体
+        String textMsg = "{ \"title\": \"title\", \"content\": \"" + msg + "\" }";
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json;charset=utf-8"),
+                textMsg);
+
+        final Request request = new Request.Builder()
+                .url(webhookUrl)
+                .addHeader("Content-Type", "application/json; charset=utf-8")
+                .post(requestBody)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d(TAG,"onFailure：" + e.getMessage());
+                SendHistory.addHistory("PushPlus转发:"+msgf+"onFailure：" + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseStr = response.body().string();
+                Log.d(TAG,"Code：" + String.valueOf(response.code())+responseStr);
+                SendHistory.addHistory("PushPlus转发:"+msgf+"Code：" + String.valueOf(response.code())+responseStr);
+            }
+        });
+    }
+}
